c8fe296e429afeb894c903f3de9d5c53a86bb2a0
Nikita <npankow93@gmail.com>
2024-01-07 12:55:20 +0300

    вывод строк массива длина которых меньше 3-х символов

--- CW1.py
+++ CW1.py
@@ -10,3 +10,7 @@ for string in inputarray:
     if len(string) <= 3:
         newarray[index] = string
         index += 1
+
+print("Массив строк длина которых меньше или равна 3 символам:")
+for string in newarray:
+    print(string, end=" ")
