b8841f67fdc17da5167d228575c58ef2306ff008
Nikita <npankow93@gmail.com>
2024-01-07 12:53:46 +0300

    создание нового массива newarray

--- CW1.py
+++ CW1.py
@@ -3,3 +3,10 @@ count = 0
 for string in inputarray:
     if len(string) <= 3:
         count += 1
+
+newarray = [None] * count
+index = 0
+for string in inputarray:
+    if len(string) <= 3:
+        newarray[index] = string
+        index += 1
